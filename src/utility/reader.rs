93053d7fafdd2845b9e16ea3b18f34605ac70cf8
John Dugan <34268+jdugan@users.noreply.github.com>
2023-08-01 19:16:54 +0200

    Day 4

--- src/utility/reader.rs
+++ src/utility/reader.rs
@@ -4,16 +4,17 @@ use std::fs::read_to_string;
 // Public Methods
 // ----------------------------------------------------
 
-pub fn to_chars(filename: &str) -> Vec<char> {
-    to_lines(filename)[0]
-        .chars()
-        .collect()
-}
-
 pub fn to_lines(filename: &str) -> Vec<String> {
     read_to_string(filename)
         .unwrap()
         .lines()
+        .map(String::from)
+        .collect()
+}
+
+pub fn split_word(word: String) -> Vec<String> {
+    word.split_terminator("")
+        .skip(1)
         .map(String::from)
         .collect()
 }
